85f651a616215c24e61b1ea632c796ef68ba8d0e
bevynisa <email.23523226@students.uii.ac.id>
2025-12-07 22:33:25 +0700

    ubah yareu di main

--- app/src/main/java/com/yareu/redconnect/MainActivity.kt
+++ app/src/main/java/com/yareu/redconnect/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     RedConnectTheme {
-        Greeting("Android")
+        Greeting("yareu")
     }
 }
